6248363e6d3c47121b56107c18b6dd7dde55fbc4
Jonathan Rosado <jon.rosado42@gmail.com>
2017-10-23 00:01:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with_index(1) { |dwarf, i| puts "#{i}. #{dwarf}" }
 end
 
-def summon_captain_planet(planeteers)
-  planeteers.collect {|element| element.capitalize << "!"}
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |element| element.capitalize << "!" }
 end
 
-def long_planeteer_calls(calls)
-  calls.any? {|call| call.length > 4}
+def long_planeteer_calls(words)
+  words.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find {|cheese| food.include?(cheese) }
+  cheese_types.find { |cheese| food.include?(cheese) }
 end
